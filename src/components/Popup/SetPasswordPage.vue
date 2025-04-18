676a64cb334b2533d97c08a290b097baee05fe44
sharkboy119812 <130572436+sharkboy119812@users.noreply.github.com>
2025-04-18 11:18:48 -0600

    Update src/components/Popup/SetPasswordPage.vue

--- src/components/Popup/SetPasswordPage.vue
+++ src/components/Popup/SetPasswordPage.vue
@@ -119,4 +119,18 @@ export default Vue.extend({
     IconTouchId
   }
 });
-</script>
+ </script># Authenticator [![Build Status](https://travis-ci.com/Authenticator-Extension/Authenticator.svg?branch=dev)](https://travis-ci.com/Authenticator-Extension/Authenticator) [![Crowdin](https://d322cqt584bo4o.cloudfront.net/authenticator-firefox/localized.svg)](https://crowdin.com/project/authenticator-firefox) <img align="right" width="100" height="100" src="https://github.com/Authenticator-Extension/Authenticator/raw/dev/images/icon.svg">
+
+> Authenticator generates 2-Step Verification codes in your browser.
+
+## Available for Chrome, Firefox, Microsoft Edge and Safari
+
+[<img src="https://raw.githubusercontent.com/wiki/Authenticator-Extension/Authenticator/readme-images/chrome-web-store.png" title="Chrome Web Store" width="170" height="48" />](https://chrome.google.com/webstore/detail/authenticator/bhghoamapcdpbohphigoooaddinpkbai) [<img src="https://raw.githubusercontent.com/wiki/Authenticator-Extension/Authenticator/readme-images/firefox-add-ons.png" title="Firefox Add-ons" width="170" height="48" />](https://addons.mozilla.org/en-US/firefox/addon/auth-helper?src=external-github) [<img src="https://raw.githubusercontent.com/wiki/Authenticator-Extension/Authenticator/readme-images/microsoft-store.png" title="Microsoft Store" height="48">](https://microsoftedge.microsoft.com/addons/detail/ocglkepbibnalbgmbachknglpdipeoio) [<img width="150" alt="Download on the App Store" src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"/>](https://apps.apple.com/us/app/authen/id1602945200?mt=12)
+
+## Build Setup
+
+``` bash
+# install development dependencies
+npm install
+# compile
+npm run [chrome, firefox, prod]
